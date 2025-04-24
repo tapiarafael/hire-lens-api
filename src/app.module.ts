ba6e3cac43d82fb38656ca10e095424e46e10c11
Rafael Tapia <rafaelsilvatapia@gmail.com>
2025-04-24 11:50:46 -0300

    chore: install pino and configure as default logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { DrizzleModule } from './drizzle/drizzle.module';
 import { StorageModule } from './storage/storage.module';
 import { ConfigModule } from '@nestjs/config';
 import { ResumeModule } from './resume/resume.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: () => ({
+          context: 'HTTP',
+        }),
+        ...(process.env.NODE_ENV !== 'production'
+          ? {
+              transport: {
+                target: 'pino-pretty',
+                options: { singleLine: true },
+              },
+            }
+          : {}),
+      },
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     DrizzleModule,
     StorageModule,
