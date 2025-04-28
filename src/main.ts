ec6d0053cfde6c1395f19eb35d386c59136dcdf3
Rafael Tapia <rafaelsilvatapia@gmail.com>
2025-04-28 15:48:31 -0300

    feat: implement HTML page to check API features

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
+  app.enableCors({
+    origin: '*', // Allow any origin for local development
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
